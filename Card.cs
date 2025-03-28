39d6f877c147582e0e7c066fed88fbef016f680b
osama0210 <osama.asmi04@gmail.com>
2025-03-28 08:53:27 +0100

    Start implementing card functionality.

---
+++ Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    class Card
+    {
+        private Suits suit;
+        private FaceValues faceValue;
+        private int cardValue;
+        private Image image;
+
+
+        public Suits Suit{
+            get { return suit; }
+        }
+
+        public FaceValues FaceValue
+        {
+            get { return faceValue; }
+        }
+
+        public int CardValue
+        {
+            get { return cardValue; }
+            set { cardValue = value; }
+        }
+
+        public Card(Suits suit, FaceValues faceValue, int cardValue)
+        {
+            suit = Suit;
+            faceValue = FaceValue;
+            cardValue = CardValue;
+        }
+    }
+}
---
+++ Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    class Deck
+    {
+        private Card cards = 
+    }
+}
